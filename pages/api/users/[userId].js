c5b04778e903f934e569b3e5a2c31971b14790c2
Joaquim Pessôa Filho <joaquimp@gmail.com>
2022-08-30 12:55:43 -0300

    Adicionado suporte a Cors para chamadas de APIs externas

--- pages/api/users/[userId].js
+++ pages/api/users/[userId].js
@@ -1,26 +1,47 @@
 import connectMongo from '../../../database/conn'
 import { getUser, putUser, deleteUser } from '../../../database/controller';
+import Cors from 'cors'
+
+const cors = Cors({
+    "origin": "*",
+    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+    "preflightContinue": false,
+    "optionsSuccessStatus": 204
+})
+
+function runMiddleware(req, res, fn) {
+    return new Promise((resolve, reject) => {
+        fn(req, res, (result) => {
+            if (result instanceof Error) {
+                return reject(result)
+            }
+            return resolve(result)
+        })
+    })
+}
 
 export default async function handler(req, res) {
-    
-        connectMongo().catch(() => res.status(405).json({ error: "Error in the Connection"}))
 
-        // type of request
-        const { method } = req
+    await runMiddleware(req, res, cors);
 
-        switch (method){
-            case "GET":
-                getUser(req, res);
-                break;
-            case 'PUT':
-                putUser(req, res)
-                break;
-            case 'DELETE':
-                deleteUser(req, res)
-                break;
-            default : 
-                res.setHeader('Allow', ['GET', 'POST', 'PUT', 'DELETE']);
-                res.status(405).end(`Method ${method} Not Allowd`)
-                break;
-        }
+    connectMongo().catch(() => res.status(405).json({ error: "Error in the Connection" }))
+
+    // type of request
+    const { method } = req
+
+    switch (method) {
+        case "GET":
+            getUser(req, res);
+            break;
+        case 'PUT':
+            putUser(req, res)
+            break;
+        case 'DELETE':
+            deleteUser(req, res)
+            break;
+        default:
+            res.setHeader('Allow', ['GET', 'POST', 'PUT', 'DELETE']);
+            res.status(405).end(`Method ${method} Not Allowd`)
+            break;
+    }
 }
--- pages/api/users/index.js
+++ pages/api/users/index.js
@@ -1,14 +1,36 @@
 import connectMongo from '../../../database/conn'
 import { getUsers, postUser, putUser, deleteUser } from '../../../database/controller';
 
+import Cors from 'cors'
+
+const cors = Cors({
+    "origin": "*",
+    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+    "preflightContinue": false,
+    "optionsSuccessStatus": 204
+})
+
+function runMiddleware(req, res, fn) {
+    return new Promise((resolve, reject) => {
+        fn(req, res, (result) => {
+            if (result instanceof Error) {
+                return reject(result)
+            }
+            return resolve(result)
+        })
+    })
+}
+
 export default async function handler(req, res) {
-    connectMongo().catch(() => res.status(405).json({ error: "Error in the Connection"}))
+
+    await runMiddleware(req, res, cors);
+    connectMongo().catch(() => res.status(405).json({ error: "Error in the Connection" }))
 
     // type of request
     const { method } = req
 
-    switch(method){
-        case 'GET' :
+    switch (method) {
+        case 'GET':
             getUsers(req, res)
             break;
         case 'POST':
@@ -20,10 +42,9 @@ export default async function handler(req, res) {
         case 'DELETE':
             deleteUser(req, res)
             break;
-        default : 
+        default:
             res.setHeader('Allow', ['GET', 'POST', 'PUT', 'DELETE']);
             res.status(405).end(`Method ${method} Not Allowd`)
             break;
     }
-  }
-  
+}
